3ab4eb023ebfc98fb65ad3cb993ab6234338a759
Eric Mayer <eric.araujo_mayer@hotmail.com>
2020-05-22 19:06:56 -0300

    Transformado a animação de scroll do contéudo das sections em classe

--- js/modules/scroll-conteudo.js
+++ js/modules/scroll-conteudo.js
@@ -1,27 +1,34 @@
-export default function scroll() {
+export default class ScrollConteudo {
   // função para adicionar a classe css que será responsável por fazer a animação do texto, conforme for descendo o conteúdo pelo scroll
 
-  const sections = document.querySelectorAll('[data-anime="conteudo"]');
-  const metade = window.innerHeight * 0.7; // pegando 70% do tamanho do heigth da tela, para definir se a section estará visível ou não
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.metade = window.innerHeight * 0.7; // calculando 70 % da tela
+    this.animaScroll = this.animaScroll.bind(this); // alterando referência
+  }
 
-  const animaScroll = () => {
-    sections.forEach((section) => {
+  animaScroll() {
+    this.sections.forEach((section) => {
       const topo = section.getBoundingClientRect().top; // metódo que retorna vários valores relacionados a distância entre eles o top
-      const visible = topo - metade < 0; // toda vez que for menor que zero irá adicionar a classe na section para que seja feita a animação do conteúdo
+      const visible = topo - this.metade < 0; // toda vez que for menor que zero irá adicionar a classe na section para que seja feita a animação do conteúdo
 
       if (visible) {
         // visible retorna true caso seja menor e false caso seja maior
         section.classList.add("ativo");
       }
     });
-  };
+  }
 
-  if (sections.length) {
-    // se a node list não for vazia
+  addEvent() {
+    window.addEventListener("scroll", this.animaScroll);
+  }
 
-    animaScroll(); // executando a função uma vez para que o contéudo seja exibido, ao executar a função uma vez o visible será menor que 0 e irá adicionar a classe ativo na primeira section, já fazendo a animação
-
-    // adicionando listener ao window objeto principal do javascript
-    window.addEventListener("scroll", animaScroll);
+  iniciar() {
+    if (this.sections.length) {
+      // verificando se existe alguma section
+      this.animaScroll(); // executando pela primeira para animar o site
+      this.addEvent(); // chamando método que adiciona evento
+    }
+    return this; // retornando referência da classe
   }
 }
