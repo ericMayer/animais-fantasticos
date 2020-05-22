073470a79f69fbbdfa95d223df794305520c23c8
Eric Mayer <eric.araujo_mayer@hotmail.com>
2020-05-22 16:26:42 -0300

    Alterado animação dos numeros para classe

--- js/modules/numeros.js
+++ js/modules/numeros.js
@@ -1,43 +1,59 @@
-export default function initNumeros() {
-  const quant = document.querySelectorAll('[data-numero="quantidade"]');
-  let stalker; // criando um observador
+export default class AnimacaoNumeros {
+  constructor(quant, stalkeadoClass, stalkerTarget) {
+    this.quant = document.querySelectorAll(quant);
+    this.stalkeadoClass = stalkeadoClass;
+    this.stalkerTarget = document.querySelector(stalkerTarget);
 
-  const animacao = () => {
-    quant.forEach((numero) => {
-      const total = +numero.innerText; // pegando texto do span e convertendo para número
-      const aux = Math.floor(total / 100); // variavel auxiliar para somar no contador para que seja mais rápido a somda dos números
+    this.modeStalker = this.modeStalker.bind(this); // alterando referência do método para a classe, pois será usado como callback
+  }
 
-      let cont = 0;
+  // let stalker; // criando um observador
 
-      const tempo = setInterval(() => {
-        cont += aux; // somando o contador
-        numero.innerText = cont; // atribuindo cont ao texto do número
+  static incremento(numero) {
+    const total = +numero.innerText; // pegando texto do span e convertendo para número
+    const aux = Math.floor(total / 100); // variavel auxiliar para somar no contador para que seja mais rápido a somda dos números
 
-        if (cont > total) {
-          // caso for maior que número significa que terminou a animação
+    let cont = 0;
 
-          numero.innerText = total; // definindo o número original como o valor do texto, pois como está somando bem rápido passa do número correto
-          clearInterval(tempo);
-        }
-      }, Math.random() * 50); // tempo será aletório
+    const tempo = setInterval(() => {
+      cont += aux; // somando o contador
+      numero.innerText = cont; // atribuindo cont ao texto do número
+
+      if (cont > total) {
+        // caso for maior que número significa que terminou a animação
+
+        numero.innerText = total; // definindo o número original como o valor do texto, pois como está somando bem rápido passa do número correto
+        clearInterval(tempo);
+      }
+    }, Math.random() * 50); // tempo será aletório
+  }
+
+  animacao() {
+    this.quant.forEach((numero) => {
+      this.constructor.incremento(numero); // para chamar uma função estática é necessário colocar a classe o this.constructor.nome do método
     });
-  };
+  }
 
-  const modeStalker = (stalkeado) => {
+  modeStalker(stalkeado) {
     // stalkeado é um array like parecido com o event, onde usuaremos o target para ver se a section já possui a classe ativo, assim irá começar a animação dos número
-    if (stalkeado[0].target.classList.contains("ativo")) {
-      stalker.disconnect(); // desligado modo stalker
-      animacao(); // chamando função para animar os números
+    if (stalkeado[0].target.classList.contains(this.stalkeadoClass)) {
+      this.stalker.disconnect(); // desligado modo stalker
+      this.animacao(); // chamando função para animar os números
     }
-  };
+  }
 
-  stalker = new MutationObserver(modeStalker);
+  addStalker() {
+    this.stalker = new MutationObserver(this.modeStalker);
 
-  if (quant) {
-    // se for true, se a lista não for vazia será true
+    this.stalker.observe(this.stalkerTarget, { attributes: true }); // definindo alvo para observar e o que será observador
+  }
 
-    const stalkerTarget = document.querySelector(".quantidade"); // selecionando o alvo que será observado
+  iniciar() {
+    // se for verdadeiro inicia mutation
+    if (this.quant.length && this.stalkerTarget) {
+      this.addStalker();
+    }
 
-    stalker.observe(stalkerTarget, { attributes: true }); // definindo alvo para observar e o que será observador
+    return this; // retornando referência da classe
   }
 }
