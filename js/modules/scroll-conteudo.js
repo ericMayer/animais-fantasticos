cd051456483b51ec60957e06e7b84f7f6849960e
Eric Mayer <eric.araujo_mayer@hotmail.com>
2020-05-22 21:57:06 -0300

    Foi refeito os métodos de scroll para não ficar pegando toda hora as posições em relação ao topo, assim o script teve uma melhora na performance

--- js/modules/scroll-conteudo.js
+++ js/modules/scroll-conteudo.js
@@ -3,32 +3,66 @@ export default class ScrollConteudo {
 
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.metade = window.innerHeight * 0.7; // calculando 70 % da tela
-    this.animaScroll = this.animaScroll.bind(this); // alterando referência
+    this.metade = window.innerHeight * 0.6; // calculando 70 % da tela
+    this.anima = this.anima.bind(this); // alterando referência
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const topo = section.getBoundingClientRect().top; // metódo que retorna vários valores relacionados a distância entre eles o top
-      const visible = topo - this.metade < 0; // toda vez que for menor que zero irá adicionar a classe na section para que seja feita a animação do conteúdo
-
-      if (visible) {
-        // visible retorna true caso seja menor e false caso seja maior
-        section.classList.add("ativo");
-      }
+  // método responsável por pegar a distância do top
+  // e diminuir menos a metade do tamanho da tela
+  // depois retorna um objeto com o top e a section
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      return {
+        top: Math.floor(section.offsetTop - this.metade),
+        element: section,
+      };
     });
   }
 
+  // verifica se o eixo Y do window é maior que
+  // o item.top que é o a distância que está do topo a section,
+  // se sim adiciona a classe ativo para que seja executado
+  // a animação
+  anima() {
+    let contador = 0;
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.top) {
+        item.element.classList.add("ativo");
+        contador += 1;
+      }
+    });
+
+    // é criado um contador que soma + 1
+    // cada vez que é adicionado a classe ativo
+    // quando esse valor for igual ao
+    // length de sections será chamado
+    // o método responsável por destruir o evento
+    // pois só será animado uma vez na página
+    if (contador === this.sections.length) {
+      this.destroyer();
+    }
+  }
+
+  // adicionando evento
   addEvent() {
-    window.addEventListener("scroll", this.animaScroll);
+    window.addEventListener("scroll", this.anima);
   }
 
   iniciar() {
+    // verificando se existe alguma section
     if (this.sections.length) {
-      // verificando se existe alguma section
-      this.animaScroll(); // executando pela primeira para animar o site
+      // pegando a distância inicial de cada section
+      // em relação ao topo
+      this.getDistance();
+
+      this.anima(); // executando pela primeira para animar o site
       this.addEvent(); // chamando método que adiciona evento
     }
     return this; // retornando referência da classe
+  }
+
+  // remove o evento
+  destroyer() {
+    window.removeEventListener("scroll", this.anima);
   }
 }
