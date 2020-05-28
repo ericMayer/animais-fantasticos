adb0a0a1f4cac1bd5ac0756165f767039e767214
Eric Mayer <eric.araujo_mayer@hotmail.com>
2020-05-28 16:03:29 -0300

    Criado eventos de touch e organizado código para ficar separado e facilitar o entendimento

--- slide/js/slide.js
+++ slide/js/slide.js
@@ -16,6 +16,8 @@ export default class Slide {
     };
   }
 
+  // -------------- Eventos de Click ----------------- //
+
   // callback de quando for clicado
   // e segurado o mouse
   mouseDown(event) {
@@ -30,6 +32,31 @@ export default class Slide {
     // para arrastar
     this.container.addEventListener("mousemove", this.mouseMove);
   }
+
+  // método quando o mouse é movido
+  mouseMove(event) {
+    // passando posição atual quando o mouse está sendo movido
+    // e armazenando numa variável
+    const posicao = this.atualizaPosicao(event.pageX);
+
+    // chamando método que fará o movimento
+    // do slide e passando a posição
+    this.moveSlide(posicao);
+  }
+
+  // quando o mouse não estiver mais clicado
+  // será removido o evento de mousemove
+  mouseUp() {
+    this.container.removeEventListener("mousemove", this.mouseMove);
+
+    // quando for deixado de clicar com o mouse
+    // será salva a posição final
+    this.move.posicaoFinal = this.move.posicaoSalva;
+  }
+
+  // -------------- Fim Eventos de Click ----------------- //
+
+  // -------------- Lógica de movimentação do Slide ----------------- //
 
   // método que atualiza a posição movida
   atualizaPosicao(pageX) {
@@ -63,40 +90,79 @@ export default class Slide {
     this.slide.style.transform = `translate3d(${posicao}px, 0, 0)`;
   }
 
-  // método quando o mouse é movido
-  mouseMove(event) {
-    // passando posição atual quando o mouse está sendo movido
+  // -------------- Fim Lógica de movimentação do Slide ----------------- //
+
+  // -------------- Eventos de Touch -----------------//
+
+  // quando iniciar o toque no slide pelo dedo
+  touchStart(event) {
+    // pegando a posição inicial do toque
+    this.move.posicaoInicial = event.changedTouches[0].pageX;
+
+    this.container.addEventListener("touchmove", this.touchMove);
+  }
+
+  // método quando é arrastado com o toque na tela
+  touchMove(event) {
+    // passando posição atual quando o dedo é arrastado na tela
     // e armazenando numa variável
-    const posicao = this.atualizaPosicao(event.pageX);
+    const posicao = this.atualizaPosicao(event.changedTouches[0].pageX);
 
     // chamando método que fará o movimento
     // do slide e passando a posição
     this.moveSlide(posicao);
   }
 
-  // quando o mouse não estiver mais clicado
-  // será removido o evento de mousemove
-  mouseUp() {
-    this.container.removeEventListener("mousemove", this.mouseMove);
+  // removendo o evento quando
+  // o dedo parar de tocar a tela
+  touchEnd() {
+    this.container.removeEventListener("touchmove", this.touchMove);
 
-    // quando for deixado de clicar com o mouse
+    // quando for deixado de tocar na tela
     // será salva a posição final
     this.move.posicaoFinal = this.move.posicaoSalva;
   }
 
+  // -------------- Fim Eventos de Touch -----------------//
+
   // alterando referências dos this dos eventos
   referencias() {
+    // -------- Referência dos métodos de click ------------ //
     this.mouseDown = this.mouseDown.bind(this);
     this.mouseMove = this.mouseMove.bind(this);
     this.mouseUp = this.mouseUp.bind(this);
+
+    // -------- Fim Referência dos métodos de click ------------ //
+
+    // -------- Referência dos métodos de touch ------------ //
+
+    this.touchStart = this.touchStart.bind(this);
+    this.touchMove = this.touchMove.bind(this);
+    this.touchEnd = this.touchEnd.bind(this);
+
+    // -------- Fim Referência dos métodos de touch ------------ //
   }
 
-  // adiciona eventos iniciais que serão usados
   addEvents() {
+    // --------- Adiciona Eventos de Click ------------------- //
+
     // evento de click e arrastar do mouse
     this.container.addEventListener("mousedown", this.mouseDown);
+
     // evento de quando o mouse deixa de estar clicado
     this.container.addEventListener("mouseup", this.mouseUp);
+
+    // --------- Fim de Adicionar Eventos de Click ------------------- //
+
+    // --------- Adiciona Eventos de Touch ------------------- //
+
+    // evento de clicar com o dedo
+    this.container.addEventListener("touchstart", this.touchStart);
+
+    // evento de quando parar de tocar com o dedo
+    this.container.addEventListener("touchend", this.touchEnd);
+
+    // --------- Fim de Adicionar Eventos de Touch ------------------- //
   }
 
   // iniciando métodos para funcionamento da classe
