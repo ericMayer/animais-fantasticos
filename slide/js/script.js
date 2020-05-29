36fd31886c8d44bc688be9e4f505096893f193e4
Eric Mayer <eric.araujo_mayer@hotmail.com>
2020-05-28 22:27:28 -0300

    Voltado arquivos corretos, pois tinha sido um merge incorreto com o slide

--- slide/js/script.js
+++ slide/js/script.js
@@ -1,1 +1,5 @@
-import debounceScroll from "../../js/modules/debounce.js";
+import Slide from "./slide.js";
+
+const slide = new Slide('[data-slide="container"]', '[data-slide="slide"]');
+slide.iniciar();
+slide.trocaSlide(3);
